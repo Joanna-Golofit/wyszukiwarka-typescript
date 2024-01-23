c54616056696c40d8195b0bd8908c06d030aaf3e
Joanna Gołofit <jgolofit@future-processing.com>
2024-01-23 14:44:14 +0100

    displaying data in two columns

--- wyszukiwarka_typescript/src/App.tsx
+++ wyszukiwarka_typescript/src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import data from "./data/02-faq.json";
+import { FAQ } from "./types/types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+	const [search, setSearch] = useState<string>("");
+	const [showContent, setShowContent] = useState<number | null>(null);
+
+	const filteredData: FAQ[] = data.questions.filter((faq: FAQ) =>
+		(faq.title + faq.content).toLowerCase().includes(search.toLowerCase())
+	);
+	console.log(data);
+	console.log(filteredData);
+
+	return (
+		<main className="FAQ">
+			<input
+				type="text"
+				value={search}
+				onChange={e => setSearch(e.target.value)}
+				placeholder="Search..."
+			/>
+			<div className="main-container">
+				<section className="left">
+					{data.groups.left.map((group, index) => (
+						<div key={index}>
+							<h2>{group.name}</h2>
+							{filteredData
+								.filter(faq => faq.groupId === group.id)
+								.map((faq, index) => (
+									<div key={index}>
+										<h3 onClick={() => setShowContent(faq.id)}>{faq.title}</h3>
+										{showContent === faq.id && (
+											<p dangerouslySetInnerHTML={{ __html: faq.content }} />
+										)}
+									</div>
+								))}
+						</div>
+					))}
+				</section>
+				<section className="right">
+					{data.groups.right.map((group, index) => (
+						<div key={index}>
+							<h2>{group.name}</h2>
+							{filteredData
+								.filter(faq => faq.groupId === group.id)
+								.map((faq, index) => (
+									<div key={index}>
+										<h3 onClick={() => setShowContent(faq.id)}>{faq.title}</h3>
+										{showContent === faq.id && (
+											<p dangerouslySetInnerHTML={{ __html: faq.content }} />
+										)}
+									</div>
+								))}
+						</div>
+					))}
+				</section>
+			</div>
+			{/* {filteredData.map((faq: FAQ, index: number) => (
+				<div key={index}>
+					<h2 onClick={() => setShowContent(faq.id)}>{faq.title}</h2>
+					{showContent === index && <p>{faq.content} </p>}
+				</div>
+			))} */}
+		</main>
+	);
+};
 
 export default App;
---
+++ wyszukiwarka_typescript/src/types/types.ts
@@ -0,0 +1,6 @@
+export type FAQ = {
+  id: number;
+  groupId: number;
+  title: string;
+  content: string;
+};
